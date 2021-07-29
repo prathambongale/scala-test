e137fb7ce8cd364e5ce604b820cdd5513579536d
prathambongale <prathambongale@hotmail.com>
2021-07-29 17:24:08 +0200

    can get mongodb record count

--- src/main/scala/org/practise/App.scala
+++ src/main/scala/org/practise/App.scala
@@ -1,34 +1,41 @@
 package org.practise
 
 /**
- * @author ${user.name}
+ * @author ${prathambongale}
  */
 object App {
   
-  def foo(x : Array[String]) = x.foldLeft("")((a,b) => a + b)
+  //def foo(x : Array[String]) = x.foldLeft("")((a,b) => a + b)
   
   def main(args : Array[String]) {
 
-    import org.apache.spark.sql.SparkSession
+    //println( "Hello World!" )
+    //println("concat arguments = " + foo(args))
 
-    val sparkSession = SparkSession
+    import org.apache.spark.sql.SparkSession
+    
+    val spark = SparkSession
       .builder()
-      .master("local")
+      .master("local[*]")
       .appName("App")
-      .config("spark.mongodb.input.uri", "mongodb://127.0.0.1/scala_spark_db.test")
-      .config("spark.mongodb.output.uri", "mongodb://127.0.0.1/scala_spark_db.test")
+      .config("spark.mongodb.input.uri", "mongodb://127.0.0.1:27017/scala_spark_db.test")
+      //.config("spark.mongodb.output.uri", "mongodb://127.0.0.1/scala_spark_db.test")
       .getOrCreate()
 
+    import spark.implicits._
+    import com.mongodb.spark.sql._
     import com.mongodb.spark._
     import com.mongodb.spark.config._
-    import org.bson.Document 
+    import org.bson.Document
+    import com.mongodb.spark.MongoSpark
+    
+    val sc = spark.sparkContext
 
-    println( "Hello World!" )
-    println("concat arguments = " + foo(args))
+    val rdd = MongoSpark.load(sc)
 
-    val df = MongoSpark.load(sparkSession)  // Uses the SparkSession
-    df.printSchema()
-    val readConfig = ReadConfig()
+    //rdd.printSchema() // Prints DataFrame schema
+    println("mongo record count : " + rdd.count)
+    //println(rdd.first.toJson)
   
   }
 
