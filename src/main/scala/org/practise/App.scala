902ef58913f4086e6166ccf70d0c29f4ff093c4f
prathambongale <prathambongale@hotmail.com>
2021-08-01 21:03:23 +0200

    writing data into parquet file successfully

--- src/main/scala/org/practise/App.scala
+++ src/main/scala/org/practise/App.scala
@@ -39,6 +39,23 @@ object App {
     println("Collection Record : ")
     rdd.foreach(x => println(x))
   
+    // Encoders for most common types are automatically provided by importing spark.implicits._
+    import spark.implicits._
+    //val peopleDF = spark.read.json("examples/src/main/resources/people.json")
+
+    // DataFrames can be saved as Parquet files, maintaining the schema information
+    df.write.parquet("people.parquet")
+
+    // Read in the parquet file created above
+    // Parquet files are self-describing so the schema is preserved
+    // The result of loading a Parquet file is also a DataFrame
+    val parquetFileDF = spark.read.parquet("people.parquet")
+
+    // Parquet files can also be used to create a temporary view and then used in SQL statements
+    parquetFileDF.createOrReplaceTempView("parquetFile")
+    val namesDF = spark.sql("SELECT name FROM parquetFile")
+    namesDF.map(attributes => "Name: " + attributes(0)).show()
+
   }
 
 }
