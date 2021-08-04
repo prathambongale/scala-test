b040f625a39391c20ef0e86102f541ee806f94dc
prathambongale <prathambongale@hotmail.com>
2021-08-04 19:28:08 +0200

    spark scala mongodb code completed

--- src/main/scala/org/practise/App.scala
+++ src/main/scala/org/practise/App.scala
@@ -3,6 +3,9 @@ package org.practise
 import com.mongodb.spark.MongoSpark
 import com.mongodb.spark.config.ReadConfig
 import org.apache.spark.{SparkConf, SparkContext}
+import org.slf4j.LoggerFactory
+import org.apache.hadoop.fs.FileSystem
+import org.apache.spark.sql.SparkSession
 
 /**
  * @author ${prathambongale}
@@ -11,33 +14,27 @@ object App {
   
   def main(args : Array[String]) {
 
-    val sc =  new SparkContext(new SparkConf())
+    // Logging
+    val Logger = LoggerFactory.getLogger(getClass().getName)
 
-    val rdd = MongoSpark.load(sc)
+    try {
 
-    println("mongo record count : " + rdd.count)
-    println("mongo list : " + rdd.collect().foreach(Document=>Unit))
-    println("mongo first record : " + rdd.first().toJson())
-    // Print All MongoDB collection record
-    println("Collection Record : ")
-    rdd.foreach(x => println(x))
+      // Configuration
+      val spark:SparkSession = SparkSession
+        .builder()
+        .getOrCreate()
+
+      // MongoDB Connection
+      val rdd = MongoSpark.load(spark)
+
+      // Write data into Hadoop Subsystem
+      rdd.toDF().write.mode("overwrite").parquet(args(0))
     
-    // Print Schema
-    rdd.toDF().printSchema()
-
-    // DataFrames can be saved as Parquet files, maintaining the schema information
-    rdd.toDF().write.parquet("people.parquet")
-
-    // Read in the parquet file created above
-    // Parquet files are self-describing so the schema is preserved
-    // The result of loading a Parquet file is also a DataFrame
-    val sqlContext = new org.apache.spark.sql.SQLContext(sc)
-    val parquetFileDF = sqlContext.read.parquet("people.parquet")
-
-    // Parquet files can also be used to create a temporary view and then used in SQL statements
-    parquetFileDF.createOrReplaceTempView("parquetFile")
-    val namesDF = sqlContext.sql("SELECT * FROM parquetFile")
-    //namesDF.map(attributes => "Name: " + attributes(0)).show()
-    namesDF.show()
+    } catch {
+      case ex: Exception => {
+        Logger.error("Exception has Occurred : ")
+        ex.printStackTrace
+      }
+    }
   }
 }
